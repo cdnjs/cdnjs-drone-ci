f259a2ca22b7cc6e3f20a9d963e7a26000588ced
Peter Dave Hello <hsu@peterdavehello.org>
2016-11-29 17:12:38 +0800

    improve sparseCheckout

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM node:4-alpine
 MAINTAINER Peter Dave Hello <hsu@peterdavehello.org>
 ENV NPM_CONFIG_LOGLEVEL warn
-RUN apk -Uuv add openssh-client rsync git sshpass
+RUN apk -Uuv add openssh-client rsync git sshpass coreutils gawk
 RUN ssh -V
 RUN git --version
 RUN node --version
--- cdnjs.sh
+++ cdnjs.sh
@@ -47,7 +47,7 @@ echo "make sure sparseCheckout enabled"
 git config core.sparseCheckout true
 
 echo "re-create sparseCheckout config"
-./tools/createSparseCheckoutConfigForCI.js
+git log --stat --stat-width=1000 ${DRONE_REPO_BRANCH}..${DRONE_COMMIT_SHA} | grep ' \| ' | awk -F'\|' '{print $1}' | grep 'ajax/libs' | awk -F'/' '{print "ajax/libs/"$3"/"$4}' | uniq >> .git/info/sparse-checkout
 
 echo "reset repository"
 git checkout -qf "${DRONE_COMMIT_SHA}"
