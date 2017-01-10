cdnjs/cdnjs-drone-ci

commit 610df04c7a7eda0c579be1e1cd9749f68e6424d5

Fix npm failed exit status, caused in 1c02f9c

cdnjs.sh
@@ -140,7 +140,10 @@ echoCyan "npm install && npm update"
 npm install && npm update
 
 echoCyan "run npm test"
-npm test -- --silent > /dev/null 2>&1 || npm test -- --color | sed 's/·//g'
+npm test -- --silent > /dev/null 2>&1 || {
+    npm test -- --color  sed 's/·//g'
+    err "npm test failed!"
+}
 
 if [ "${DRONE_COMMIT_BRANCH}" = "master" ] && [ "${DRONE_BUILD_EVENT}" = "push" ]; then
     sshpass -e ssh -oStrictHostKeyChecking=no -l "${CDNJS_CACHE_USERNAME}" "${CDNJS_CACHE_HOST}" mkdir -p "${BASEPATH}" > /dev/null 2>&1

commit b6f76dab0b3a0bb78578dfc68534a9ddb6f7e935

Fix npm test again ... wrong syntax caused in 610df04c7a7e

cdnjs.sh
@@ -141,7 +141,7 @@ npm install && npm update
 
 echoCyan "run npm test"
 npm test -- --silent > /dev/null 2>&1 || {
-    npm test -- --color  sed 's/·//g'
+    npm test -- --color | sed 's/·//g'
     err "npm test failed!"
 }
 